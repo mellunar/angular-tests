a2e3d31cc2205d9da5646700c32b8cafeaee29dd
mellunar <melfer100@gmail.com>
2025-06-23 11:50:28 -0300

    cloudflare wrangler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SpaceNews } from './components/space-news-item/space-news-item.interface';
-import { HttpCalls } from './services/http-calls';
 import { SpaceNewsList } from './components/space-news-list/space-news-list';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, SpaceNewsList],
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
-export class App {}
+export class AppComponent {}
